fffc78de38a0467e6066a9437f1b13d30cf36dbf
Tailane2517 <tailanesantos1207@hotmail.com>
2025-02-17 18:42:04 +0000

    Salvando

---
+++ 4. solicitando_dado.py
@@ -0,0 +1,11 @@
+import os
+os.system("clear")
+
+# Solicitando dados.
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+
+print(f"Nome: {nome}")
+print(f"idade: {idade}")
+print(f"altura: {altura}")
